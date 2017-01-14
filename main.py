c092612f9ad4992d2a838ed98fdcf72819bcf06e
Max Harris <mh568@kent.ac.uk>
2017-01-14 20:36:28 +0000

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+import gym
+from gym import wrappers
+import numpy as np
+import secret_constants
+
+NUMBER_OF_EPISODES = 5000
+ATTEMPTS_AT_EACH_EPISODE = 1000
+MOVE_UP = 3
+MOVE_DOWN = 1
+MOVE_RIGHT = 2
+MOVE_LEFT = 0
+LEARNING_RATE = 0.85
+NOISE = 0.99
+
+
+def main():
+	env = gym.make("FrozenLake-v0")
+	env = wrappers.Monitor(env, "/tmp/gym-results", force=True)
+	
+	reward_list = []
+	#16 * 4 - 16 places on lake and 4 possible moves
+	Q = np.zeros([env.observation_space.n,env.action_space.n])
+
+	for i in range(NUMBER_OF_EPISODES):
+		state = env.reset()
+		total_reward = 0
+		for j in range(ATTEMPTS_AT_EACH_EPISODE):
+			action = np.argmax(Q[state,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+			env.render()
+			new_state, reward, done, _ = env.step(action)
+			#update Q-table with knowledge gained
+			Q[state,action] = Q[state,action] + LEARNING_RATE*(reward + NOISE*np.max(Q[new_state,:]) - Q[state,action])
+			total_reward += reward
+			state = new_state
+			if done:
+				break
+		reward_list.append(total_reward)
+
+	print("Average score per episode: " + str(sum(reward_list)/NUMBER_OF_EPISODES))
+	print("Final Q-Table Values")
+	print(Q)
+
+	env.close()
+	gym.upload("/tmp/gym-results", api_key=secret_constants.API_KEY)
+
+if __name__ == "__main__": main()
+
+
